6d03a1514fe91d5d78855c13891db220e7760b4b
Livelyoneweek <wlvvwhs@naver.com>
2022-03-24 18:22:03 +0900

    0324 배포자동화 test

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -9,17 +9,16 @@ function find_idle_profile()
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
-        CURRENT_PROFILE=$(curl -s http://localhost/profile)
-#        CURRENT_PROFILE=real2
+        CURRENT_PROFILE=real2
     else
         CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
 
     if [ ${CURRENT_PROFILE} == real1 ]
     then
-      IDLE_PROFILE=real2
-    else
       IDLE_PROFILE=real1
+    else
+      IDLE_PROFILE=real2
     fi
 
     echo "${IDLE_PROFILE}"
