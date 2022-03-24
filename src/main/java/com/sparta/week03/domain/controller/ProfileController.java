ecad8ecfb569573a01855b57e3a5d70d0c7fa8db
Livelyoneweek <wlvvwhs@naver.com>
2022-03-24 16:30:10 +0900

    0324 배포자동화 test

--- src/main/java/com/sparta/week03/domain/controller/ProfileController.java
+++ src/main/java/com/sparta/week03/domain/controller/ProfileController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -13,11 +14,18 @@ public class ProfileController {
 
     private final Environment env;
 
-    // 프로젝트의 환경설정 값을 다루는 Environment Bean을 DI 받아 현재 활성화된 Profile을 반환
+    /* NginX가 어느 포트의 서버를 바라보고있는지 확인하기 위한 컨트롤러 */
     @GetMapping("/profile")
     public String profile() {
-        return Arrays.stream(env.getActiveProfiles())
-                .findFirst()
-                .orElse("");
+
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real1", "real2");
+        String defaultProfile = profiles.isEmpty() ? "default"
+                : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 }
